6127d8d7fd162c8d812f30721799a9cb5f689923
Nguyên Sỹ Đăng <ndang3209@gmail.com>
2025-09-28 00:52:51 +0700

    27-28/9
    Hàn thành chức năng chuyển đổi scene mà vẫn dữ đc âm thanh của menu và đã xong phần điều chỉnh ấm thanh gồm âm thanh menu và âm thanh game

--- Assets/Script/AudioSettings.cs
+++ Assets/Script/AudioSettings.cs
@@ -11,24 +11,30 @@ public class AudioSettings : MonoBehaviour
 
     void Start()
     {
-        float masterVolume, musicVolume;
-        audioMixer.GetFloat("MasterVolume", out masterVolume);
-        audioMixer.GetFloat("MusicVolume", out musicVolume);
+        float masterValue = PlayerPrefs.GetFloat("MasterVolume", 0.75f);
+        float musicValue = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
 
-        masterSlider.value = Mathf.Pow(10, masterVolume / 20f);
-        musicSlider.value = Mathf.Pow(10, musicVolume / 20f);
+        masterSlider.value = masterValue;
+        musicSlider.value = musicValue;
 
         masterSlider.onValueChanged.AddListener(SetMasterVolume);
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
+
+        SetMasterVolume(masterValue);
+        SetMusicVolume(musicValue);
     }
 
     public void SetMasterVolume(float value)
     {
-        audioMixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20);
+        float dB = Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20;
+        audioMixer.SetFloat("MasterVolume", dB);
+        PlayerPrefs.SetFloat("MasterVolume", value);
     }
 
     public void SetMusicVolume(float value)
     {
-        audioMixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);
+        float dB = Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20;
+        audioMixer.SetFloat("MusicVolume", dB);
+        PlayerPrefs.SetFloat("MusicVolume", value);
     }
 }
