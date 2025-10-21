1c4f35e489d9b4a2eef85bb94500863a3e6713a6
NguyenSumie05 <169138503+NguyenSumie05@users.noreply.github.com>
2025-10-21 12:38:55 +0700

    upp

---
+++ Assets/Script/RespawnObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class RespawnObject : MonoBehaviour
+{
+    private Vector3 startPosition;   // Lưu vị trí ban đầu
+    private Quaternion startRotation; // Lưu hướng ban đầu
+
+    void Start()
+    {
+        // Ghi nhớ vị trí + hướng ban đầu của object
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+    }
+
+    public void Respawn()
+    {
+        // Đưa object về lại vị trí ban đầu
+        transform.position = startPosition;
+        transform.rotation = startRotation;
+    }
+}
