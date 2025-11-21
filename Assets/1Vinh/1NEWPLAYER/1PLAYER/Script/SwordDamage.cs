17118aaba3af22f542302e3a17f380b76b824d4f
Nguyễn Văn Vương Khang <vinhvqps38969@gmail.com>
2025-11-21 16:52:53 +0700

    UPTATE SwordDamage

--- Assets/1Vinh/1NEWPLAYER/1PLAYER/Script/SwordDamage.cs
+++ Assets/1Vinh/1NEWPLAYER/1PLAYER/Script/SwordDamage.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class SwordDamage : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float damage = 20f;
+    private bool canDamage = false;  // chỉ gây damage khi hoạt ảnh chém đang active
+
+    public void EnableDamage()  // gọi từ Animation Event
     {
-        
+        canDamage = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DisableDamage() // gọi từ Animation Event
     {
-        
+        canDamage = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!canDamage) return;
+
+        IDamageable target = other.GetComponent<IDamageable>();
+        if (target != null)
+        {
+            Debug.Log("⚔ Kiếm hit enemy!");
+            target.TakeDamage(damage);
+        }
     }
 }
