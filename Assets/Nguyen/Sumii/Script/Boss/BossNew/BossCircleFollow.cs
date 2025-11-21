c8474b1498a9e3cb52f4897808c4026d9b704b3b
NguyenSumie05 <169138503+NguyenSumie05@users.noreply.github.com>
2025-11-21 09:56:29 +0700

    up

---
+++ Assets/Nguyen/Sumii/Script/Boss/BossNew/BossCircleFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BossCircleFollow : MonoBehaviour
+{
+    public Transform boss;     // boss cần bám theo
+    public float offsetY = 0.05f;  // cao 0.05 trên mặt đất
+    public float rotateSpeed = 60f; // tốc độ xoay
+
+    void LateUpdate()
+    {
+        if (boss == null) return;
+
+        // Luôn ở dưới chân boss
+        transform.position = new Vector3(
+            boss.position.x,
+            boss.position.y + offsetY,
+            boss.position.z
+        );
+
+        // Xoay vòng tròn
+        transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+    }
+}
