2c30735ec275a1e34d7f5911b1243056ae091a08
Peter <manhphong02@gmail.com>
2025-11-24 13:09:39 +0700

    update trap va mini

---
+++ Assets/Phong/Script/Destroyer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    // Nhớ tích vào "Is Trigger" ở Collider của điểm Z
+    private void OnTriggerEnter(Collider other)
+    {
+        // Nếu cái gì chạm vào vùng này, xóa nó đi
+        if (other.CompareTag("Trap")) // Nhớ đặt Tag cho bóng là "Trap"
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
