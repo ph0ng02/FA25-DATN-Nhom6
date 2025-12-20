54f309d95a3aa368ac6cf7ddabb25ace864f1fc9
Nguyễn Văn Vương Khang <vinhvqps38969@gmail.com>
2025-12-21 00:09:46 +0700

    a

--- Assets/1Vinh/Script/GoalTile.cs
+++ Assets/1Vinh/Script/GoalTile.cs
@@ -2,34 +2,64 @@ using UnityEngine;
 
 public class GoalTile : MonoBehaviour
 {
-    public int goalID = 0;
-    public bool isFilledCorrectly = false;
+    [Header("Goal Settings")]
+    public int goalID;
+    public bool isFilledCorrectly;
+
+    [Header("Color Settings")]
+    public Color normalColor = Color.white;
+    public Color correctColor = Color.green;
+
+    private Renderer rend;
+    private PushableRock currentRock;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.color = normalColor;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Pushable"))
-        {
-            var rock = other.GetComponent<PushableRock>();
+        if (!other.CompareTag("Pushable")) return;
 
-            if (rock != null && rock.isCorrectRock && rock.correctGoalID == goalID)
-            {
-                isFilledCorrectly = true;
-            }
-            else
-            {
-                isFilledCorrectly = false;
-            }
+        PushableRock rock = other.GetComponent<PushableRock>();
+        if (rock == null) return;
+
+        currentRock = rock;
+
+        if (rock.isCorrectRock && rock.correctGoalID == goalID)
+        {
+            isFilledCorrectly = true;
+            SetColor(correctColor);
+        }
+        else
+        {
+            isFilledCorrectly = false;
+            SetColor(normalColor);
+        }
+
+        PuzzleManager.Instance.CheckPuzzle();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Pushable")) return;
+
+        PushableRock rock = other.GetComponent<PushableRock>();
+
+        if (rock == currentRock)
+        {
+            currentRock = null;
+            isFilledCorrectly = false;
+            SetColor(normalColor);
 
             PuzzleManager.Instance.CheckPuzzle();
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    void SetColor(Color color)
     {
-        if (other.CompareTag("Pushable"))
-        {
-            isFilledCorrectly = false;
-            PuzzleManager.Instance.CheckPuzzle();
-        }
+        rend.material.color = color;
     }
 }
